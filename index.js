8bfadab738df296511d7dbc799153acaa524da89
shopifydev-gy <shopifydev@gmail.com>
2024-02-23 16:27:16 +0200

    Add playground files

---
+++ index.js
@@ -0,0 +1,25 @@
+import ffmpeg from "ffmpeg";
+
+try {
+  const process = new ffmpeg("../mov_bbb.mp4");
+  process.then(
+    (video) => {
+      video.addCommand("-y");
+      video.addCommand("-vf", "scale=1920:-1");
+      video.addCommand("-movflags", "faststart");
+      video.addCommand("-vcodec", "libx264");
+      video.addCommand("-crf", "23");
+      video.addCommand("-g", "1");
+      video.addCommand("-an");
+      video.addCommand("-pix_fmt", "yuv420p");
+      video.save("./assets/mov_bbb.mp4", (error, file) => {
+        if (error) return console.log("Error: " + error);
+        console.log("Video file: " + file);
+      });
+    },
+    (error) => console.log("Error: " + error)
+  );
+} catch (error) {
+  console.log(error.code);
+  console.log(error.msg);
+}
