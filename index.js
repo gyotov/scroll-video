378995e3c55b686c947744604eef6ecc32ead861
shopifydev-gy <shopifydev@gmail.com>
2024-03-07 11:02:45 +0200

    Add latest modifications

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 import ffmpeg from "ffmpeg";
 
 try {
-  const process = new ffmpeg("../mov_bbb.mp4");
+  const process = new ffmpeg(
+    "../vecteezy_abstract-neon-lights-glow-hole-tunnel-3d-illustration-vj-loop_1908965.mp4"
+  );
   process.then(
     (video) => {
       video.addCommand("-y");
       video.addCommand("-vf", "scale=1920:-1");
       video.addCommand("-movflags", "faststart");
       video.addCommand("-vcodec", "libx264");
-      video.addCommand("-crf", "23");
+      video.addCommand("-crf", "15");
       video.addCommand("-g", "1");
       video.addCommand("-an");
       video.addCommand("-pix_fmt", "yuv420p");
-      video.save("./assets/mov_bbb.mp4", (error, file) => {
-        if (error) return console.log("Error: " + error);
-        console.log("Video file: " + file);
-      });
+      video.save(
+        "./assets/vecteezy_abstract-neon-lights-glow-hole-tunnel-3d-illustration-vj-loop_1908965.mp4",
+        (error, file) => {
+          if (error) return console.log("Error: " + error);
+          console.log("Video file: " + file);
+        }
+      );
     },
     (error) => console.log("Error: " + error)
   );
