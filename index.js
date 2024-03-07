ea9f41abafd5fd95d46b3f8fe5958bb0a3596854
shopifydev-gy <shopifydev@gmail.com>
2024-03-07 16:20:21 +0200

    Add updates

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ try {
     (video) => {
       video.addCommand("-y");
       video.addCommand("-vf", "scale=1920:-1");
-      video.addCommand("-r", "25");
+      // video.addCommand("-r", "25"); // Limit frame rate if necessary
       video.addCommand("-movflags", "faststart");
       video.addCommand("-vcodec", "libx264");
-      video.addCommand("-crf", "25");
+      video.addCommand("-crf", "25"); // Video quality - 25 equals 75%
       video.addCommand("-g", "1");
       video.addCommand("-an");
       video.addCommand("-pix_fmt", "yuv420p");
